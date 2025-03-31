48af73be655d334b59babdaf870ef69037fffcfc
Yellow <joemazloum953@gmail.com>
2025-03-31 13:01:02 +0300

    date fix

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -55,4 +55,86 @@ class HomeController extends Controller
             return redirect()->route($res->link);
         }
     }
+
+    // public function fix()
+    // {
+    //     $invoices = Invoice::all();
+    //     foreach ($invoices as $invoice) {
+    //         $datetime = new Carbon($invoice->date);
+
+    //         $invoice->update([
+    //             'created_at' => $datetime,
+    //             'updated_at' => $datetime,
+    //         ]);
+
+    //         foreach ($invoice->items as $item) {
+    //             $item->update([
+    //                 'created_at' => $datetime,
+    //                 'updated_at' => $datetime,
+    //             ]);
+    //         }
+
+    //         if ($invoice->transactions) {
+    //             foreach (explode('|', $invoice->transactions) as $id) {
+    //                 if ($id != '') {
+    //                     Transaction::find($id)->update([
+    //                         'created_at' => $datetime,
+    //                         'updated_at' => $datetime,
+    //                     ]);
+    //                 }
+    //             }
+    //         }
+    //     }
+
+    //     $receipts = Receipt::all();
+    //     foreach ($receipts as $receipt) {
+    //         $datetime = new Carbon($receipt->date);
+
+    //         $receipt->update([
+    //             'created_at' => $datetime,
+    //             'updated_at' => $datetime,
+    //         ]);
+
+    //         foreach ($receipt->items as $item) {
+    //             $item->update([
+    //                 'created_at' => $datetime,
+    //                 'updated_at' => $datetime,
+    //             ]);
+    //         }
+
+    //         if ($receipt->transactions) {
+    //             foreach (explode('|', $receipt->transactions) as $id) {
+    //                 if ($id != '') {
+    //                     Transaction::find($id)->update([
+    //                         'created_at' => $datetime,
+    //                         'updated_at' => $datetime,
+    //                     ]);
+    //                 }
+    //             }
+    //         }
+    //     }
+
+    //     $payments = Payment::all();
+    //     foreach ($payments as $payment) {
+    //         $datetime = new Carbon($payment->date);
+
+    //         $payment->update([
+    //             'created_at' => $datetime,
+    //             'updated_at' => $datetime,
+    //         ]);
+
+    //         if ($payment->transactions) {
+    //             foreach (explode('|', $payment->transactions) as $id) {
+    //                 if ($id != '') {
+    //                     Transaction::find($id)->update([
+    //                         'created_at' => $datetime,
+    //                         'updated_at' => $datetime,
+    //                     ]);
+    //                 }
+    //             }
+    //         }
+    //     }
+
+    //     return 'success';
+    // }
 }
