5579276b23974d8c543f45d65827b258a9b51c73
Yellow <joemazloum953@gmail.com>
2025-04-04 01:36:22 +0300

    fix invoice fs size

--- resources/views/invoices/show.blade.php
+++ resources/views/invoices/show.blade.php
@@ -15,7 +15,7 @@
 
         <div class="row my-5 px-3 d-flex">
             <div class="col border-custom p-0 d-flex flex-column">
-                <table class="w-100 m-0 flex-grow-1">
+                <table class="w-100 m-0 flex-grow-1 fs-20">
                     <tr class="border-none">
                         <th class="w-custom p-2 align-content-start">Due From</th>
                         <td colspan="2" class="py-2 px-4 border-left align-content-start">
@@ -43,7 +43,7 @@
             <div class="col-1"></div>
 
             <div class="col border-custom p-0 d-flex flex-column">
-                <table class="w-100 m-0 flex-grow-1">
+                <table class="w-100 m-0 flex-grow-1 fs-20">
                     <tr class="border-none">
                         <th class="w-custom p-1">Date</th>
                         <td colspan="2" class="py-1 px-4 border-left">
@@ -100,41 +100,41 @@
         <div>
             <div class="border-custom">
                 <div class="d-flex border-bottom">
-                    <div class="col-9 p-2 fw-bold">Description</div>
-                    <div class="col-3 border-left p-2 fw-bold">Amount ({{ $invoice->currency->code }})</div>
+                    <div class="col-9 p-2 fw-bold fs-20">Description</div>
+                    <div class="col-3 border-left p-2 fw-bold fs-20">Amount ({{ $invoice->currency->code }})</div>
                 </div>
 
                 <div class="w-100 min-h">
                     @foreach ($items as $item)
                     @if ($item->type == 'item')
                     <div class="row-flex w-100" style="max-height: 50px;">
-                        <div class="col-9 p-2 text-center" style="font-size: 20px;">{{ $item->description }}</div>
-                        <div class="col-3 p-2 border-left">{{ number_format($item->total_price, 2) }}</div>
+                        <div class="col-9 p-2 text-center fs-20">{{ $item->description }}</div>
+                        <div class="col-3 p-2 border-left fs-20">{{ number_format($item->total_price, 2) }}</div>
                     </div>
                     @endif
                     @endforeach
                     <div class="row-flex">
-                        <div class="col-9 p-2 fw-bold">&nbsp;</div>
-                        <div class="col-3 p-2 border-left fw-bold">&nbsp;</div>
+                        <div class="col-9 p-2 fw-bold fs-20">&nbsp;</div>
+                        <div class="col-3 p-2 border-left fw-bold fs-20">&nbsp;</div>
                     </div>
                 </div>
 
                 {{-- <div class="d-flex border-top">
-                    <div class="col-9 p-2">Sub Total</div>
-                    <div class="col-3 p-2 border-left">{{ number_format($total, 2) }}</div>
+                    <div class="col-9 p-2 fs-20">Sub Total</div>
+                    <div class="col-3 p-2 fs-20 border-left">{{ number_format($total, 2) }}</div>
                 </div>
                 <div class="d-flex border-top">
-                    <div class="col-9 p-2">VAT</div>
-                    <div class="col-3 p-2 border-left">{{ number_format($vat, 2) }}</div>
+                    <div class="col-9 p-2 fs-20">VAT</div>
+                    <div class="col-3 p-2 fs-20 border-left">{{ number_format($vat, 2) }}</div>
                 </div> --}}
                 <div class="d-flex border-top">
-                    <div class="col-9 p-2">Total</div>
-                    <div class="col-3 p-2 border-left">{{ number_format($total_price_after_vat, 2) }}</div>
+                    <div class="col-9 p-2 fs-20">Total</div>
+                    <div class="col-3 p-2 fs-20 border-left">{{ number_format($total_price_after_vat, 2) }}</div>
                 </div>
             </div>
 
             <div class="mt-5">
-                <h5>{{ Helper::format_currency_to_words($total_price_after_vat) }}</h5>
+                <div class="fs-20">{{ Helper::format_currency_to_words($total_price_after_vat) }}</div>
             </div>
         </div>
     </div>
